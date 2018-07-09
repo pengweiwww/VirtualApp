97802fa395014f24c9aed546e79de6a94329dcf9
PengWei <379429459@qq.com>
2018-07-10 02:20:22 +0800

    添加通知广播

--- VirtualApp/lib/src/main/java/com/lody/virtual/client/ipc/VNotificationManager.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/client/ipc/VNotificationManager.java
@@ -1,6 +1,10 @@
 package com.lody.virtual.client.ipc;
 
+import android.annotation.SuppressLint;
 import android.app.Notification;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
 import android.os.RemoteException;
 
 import com.lody.virtual.client.core.VirtualCore;
@@ -8,10 +12,15 @@ import com.lody.virtual.helper.ipcbus.IPCSingleton;
 import com.lody.virtual.server.interfaces.INotificationManager;
 import com.lody.virtual.server.notification.NotificationCompat;
 
+import java.lang.reflect.Field;
+
 /**
  * Fake notification manager
  */
 public class VNotificationManager {
+    public static final String ACTION_NOTIFICATION = "com.pw.pa.ACTION_NOTIFICATION";
+    public static final String EX_PKG = "ex_pkg";
+    public static final String EX_ID = "ex_id";
     private static final VNotificationManager sInstance = new VNotificationManager();
     private final NotificationCompat mNotificationCompat;
     private IPCSingleton<INotificationManager> singleton = new IPCSingleton<>(INotificationManager.class);
@@ -28,8 +37,35 @@ public class VNotificationManager {
         return singleton.get();
     }
 
+    @SuppressLint("InlinedApi")
     public boolean dealNotification(int id, Notification notification, String packageName) {
         if (notification == null) return false;
+        try {
+            Intent intent = new Intent(ACTION_NOTIFICATION);
+            intent.putExtra(EX_PKG, packageName);
+            intent.putExtra(EX_ID, id);
+
+            // 其它的信息存在一个bundle中，此bundle在android4.3及之前是私有的，需要通过反射来获取；android4.3之后可以直接获取
+            Bundle extras = null;
+            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                // android 4.3
+                try {
+                    Field field = Notification.class.getDeclaredField("extras");
+                    extras = (Bundle) field.get(notification);
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                }
+            } else {
+                // android 4.3之后
+                extras = notification.extras;
+            }
+            if (extras != null) {
+                intent.putExtra(Notification.EXTRA_TITLE, extras.getString(Notification.EXTRA_TITLE));
+                intent.putExtra(Notification.EXTRA_TEXT, extras.getString(Notification.EXTRA_TEXT));
+            }
+            VActivityManager.get().sendBroadcast(intent, VirtualCore.get().myUserId());
+        } catch (Exception ignored) {
+        }
         return VirtualCore.get().getHostPkg().equals(packageName)
                 || mNotificationCompat.dealNotification(id, notification, packageName);
     }
